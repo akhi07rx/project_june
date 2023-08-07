98a78b50a736f46d90056b00fd4f874695bcdc96
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-08-07 14:43:44 +0530

    Initial Commit

--- lib/class work/Gridex/gridview1.dart
+++ lib/class work/Gridex/gridview1.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 
-class Gridview1 extends StatelessWidget {
+void main() {
+  runApp(MaterialApp(
+    home: GridView1(),
+  ));
+}
+
+class GridView1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("My Gridview"),
+        title: const Text("My GridView"),
+      ),
+      body: GridView(
+        gridDelegate:
+            // const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 60),
+        children: List.generate(
+            15,
+            (index) => const Card(
+                  color: Colors.amber,
+                  child: Center(
+                    child: Icon(Icons.ac_unit_rounded),
+                  ),
+                )),
       ),
     );
   }
---
+++ lib/class work/Gridex/gridview3.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: GrideView3s(),
+  ));
+}
+
+class GrideView3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("GridView Count"),
+      ),
+      body: GridView.count(
+        crossAxisCount: 3,
+        children: List.generate(
+            15,
+            (index) => const Card(
+                  child: Center(
+                    child: Text("Hello"),
+                  ),
+                )),
+      ),
+    );
+  }
+}
