54ce4353c615940b26747700f25f1c6e43cbea0c
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-08-09 18:31:52 +0530

    Initial Commit

--- lib/assignment/shopping.dart
+++ lib/assignment/shopping.dart
@@ -2,27 +2,28 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: shrine(),
   ));
 }
 
 class shrine extends StatelessWidget {
   var name = [
-    'Vagabond sack',
-    'stella',
-    'Whitney belt',
-    'Garden strand',
+    'Vagabond Sack',
+    'Stella Eyeglasses',
+    'Whitney Belt',
+    'H & M Oxford Blue Shirt',
     'Strut earrings',
     'Varsity socks'
   ];
   var price = [120, 58, 35, 98, 34, 12];
   var image = [
-    'assets/shopping/sack.jpg',
-    'assets/shopping/glass.jpg',
+    'assets/shopping/sack.png',
+    'assets/shopping/glass.png',
     'assets/shopping/belt.png',
-    'assets/shopping/chain.jpeg',
-    'assets/shopping/earring.jpeg',
-    'assets/shopping/socks.jpeg'
+    'assets/shopping/shirt.png',
+    'assets/shopping/earring.png',
+    'assets/shopping/socks.png'
   ];
 
   @override
@@ -30,13 +31,13 @@ class shrine extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading: const Icon(Icons.menu),
-        title: const Text("SHRINE"),
+        title: const Text("shrine"),
         backgroundColor: Colors.indigoAccent,
         actions: const [
           Icon(Icons.search),
-          SizedBox(
-            width: 30,
-          ),
+          SizedBox(width: 10),
+          Icon(Icons.shopify),
+          SizedBox(width: 10),
           //Icon(Icons.)
         ],
       ),
