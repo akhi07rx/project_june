1d9077da933e2b5cf942a2b2f6103ff63f2bb8e1
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-08-09 14:27:40 +0530

    Initial Commit

--- lib/class work/Gridex/stag_grid.dart
+++ lib/class work/Gridex/stag_grid.dart
@@ -1,14 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: Stag_Grid(),
+  ));
+}
+
 class Stag_Grid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Staggered GridView"),
+        title: const Text("Staggered GridView"),
       ),
-      body: SingleChildScrollView(),
+      body: SingleChildScrollView(
+        child: StaggeredGrid.count(
+          crossAxisCount: 4,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          children: [
+            StaggeredGridTile.count(
+                crossAxisCellCount: 2, //width
+                mainAxisCellCount: 2, //height
+                child: Container(
+                  color: Colors.green,
+                  child: const Center(child: Icon(Icons.widgets)),
+                )),
+            StaggeredGridTile.count(
+                crossAxisCellCount: 2, //width
+                mainAxisCellCount: 4, //height
+                child: Container(
+                  color: Colors.orange,
+                  child: const Center(child: Icon(Icons.wifi)),
+                )),
+            StaggeredGridTile.count(
+                crossAxisCellCount: 1, //width
+                mainAxisCellCount: 2, //height
+                child: Container(
+                  color: Colors.blue,
+                  child: const Center(child: Icon(Icons.map)),
+                )),
+            StaggeredGridTile.count(
+                crossAxisCellCount: 1, //width
+                mainAxisCellCount: 1, //height
+                child: Container(
+                  color: Colors.purple,
+                  child: const Center(child: Icon(Icons.bluetooth)),
+                )),
+            StaggeredGridTile.count(
+                crossAxisCellCount: 1, //width
+                mainAxisCellCount: 1, //height
+                child: Container(
+                  color: Colors.pink,
+                  child: const Center(child: Icon(Icons.send)),
+                )),
+            StaggeredGridTile.count(
+                crossAxisCellCount: 4, //width
+                mainAxisCellCount: 2, //height
+                child: Container(
+                  color: Colors.red,
+                  child: const Center(child: Icon(Icons.radio)),
+                )),
+          ],
+        ),
+      ),
     );
   }
 }
