5094896db8ae952757fdd094a1af3ceac5e31dbf
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-08-11 10:36:55 +0530

    Initial Commit

--- lib/practice_projects/cardui2.dart
+++ lib/practice_projects/cardui2.dart
@@ -26,7 +26,12 @@ class StackEx extends StatelessWidget {
                   image: const DecorationImage(
                       fit: BoxFit.cover,
                       image: NetworkImage(
-                          "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a44778ea-3457-40e0-8979-b7e3685d23d0/dfph288-3bd7b361-38d3-4d73-905d-cd36ef012f42.png/v1/fit/w_828,h_464,q_70,strp/gojo_satoru_x_mobile_legends_8k_wallpaper_by_newjer53_dfph288-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTA3NCIsInBhdGgiOiJcL2ZcL2E0NDc3OGVhLTM0NTctNDBlMC04OTc5LWI3ZTM2ODVkMjNkMFwvZGZwaDI4OC0zYmQ3YjM2MS0zOGQzLTRkNzMtOTA1ZC1jZDM2ZWYwMTJmNDIucG5nIiwid2lkdGgiOiI8PTE5MjAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.owKC1nlnYRE_vYbc6qX3I-2xeYKqOJjZh--aqiYpffk"
+                          //
+                          "https://i.imgur.com/1CzkZqK.png"
+                          //"https://i.imgur.com/RNLZyuw.jpg"
+                          // "https://i.imgur.com/wkEUGX2.jpg"
+                          //"https://i.imgur.com/5mC6884.jpg"
+                          //"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a44778ea-3457-40e0-8979-b7e3685d23d0/dfph288-3bd7b361-38d3-4d73-905d-cd36ef012f42.png/v1/fit/w_828,h_464,q_70,strp/gojo_satoru_x_mobile_legends_8k_wallpaper_by_newjer53_dfph288-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTA3NCIsInBhdGgiOiJcL2ZcL2E0NDc3OGVhLTM0NTctNDBlMC04OTc5LWI3ZTM2ODVkMjNkMFwvZGZwaDI4OC0zYmQ3YjM2MS0zOGQzLTRkNzMtOTA1ZC1jZDM2ZWYwMTJmNDIucG5nIiwid2lkdGgiOiI8PTE5MjAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.owKC1nlnYRE_vYbc6qX3I-2xeYKqOJjZh--aqiYpffk"
 
                           // "https://64.media.tumblr.com/295275baf0fed1403338f3341b4b2ffd/7c806480f72c74de-c8/s540x810/b5299c4d3d1093fa6b23693e0882ea13bffaf0a5.png"
                           ))), // color: Colors.green,
