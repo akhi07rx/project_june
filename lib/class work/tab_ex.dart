18d3cd9ceb81a820fd55cbf6fbabc4bc3b359813
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-08-12 09:56:00 +0530

    Initial Commit

--- lib/class work/tab_ex.dart
+++ lib/class work/tab_ex.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: TabEx(),
+  ));
+}
+
 class TabEx extends StatelessWidget {
   const TabEx({super.key});
 
