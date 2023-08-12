de75f1c289fd7f171f1d1bebddb96bf1ce7e01b7
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-08-12 11:02:59 +0530

    Initial Commit

--- lib/class work/bottomnav.dart
+++ lib/class work/bottomnav.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(
+    home: BottomNavBar(),
+  ));
+}
+
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
 
@@ -8,19 +14,29 @@ class BottomNavBar extends StatefulWidget {
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
+  int index = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Bottom Nav Bar"),
+        title: Text("Bottom Navigation Bar"),
       ),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle_rounded), label: "Search"),
-        BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
-      ]),
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.purple,
+          selectedItemColor: Colors.yellow,
+          currentIndex: index,
+          onTap: (tappedindex) {
+            index = tappedindex;
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle_rounded), label: 'Profile'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.add_box_rounded), label: 'create')
+          ]),
     );
   }
 }
