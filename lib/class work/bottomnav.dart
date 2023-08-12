a702e3d43e95f22cfdc5c2cda871819bbb87a8e2
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-08-12 10:50:52 +0530

    Initial Commit

---
+++ lib/class work/bottomnav.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Bottom Nav Bar"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_rounded), label: "Search"),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+      ]),
+    );
+  }
+}
