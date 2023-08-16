dc0fc473e24f543fd29ef836cc2cf2ce810a15c4
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-08-16 13:59:07 +0530

    Initial Commit

--- lib/class work/bottomsheett.dart
+++ lib/class work/bottomsheett.dart
@@ -1,24 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: BottomSheett(),
+  ));
+}
 
 class BottomSheett extends StatelessWidget {
-  const BottomSheett({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(" Bottom sheet with image on tap"),
+        title: Text('BottomSheet with image on tap'),
       ),
       body: Center(
-          child: GestureDetector(
-        //we can use inkwell also for preforming events
-        onLongPress: () => showSheet(), //{},
-        child: Image.asset("assets/images/megumi.jpg"),
-      )),
+        child: GestureDetector(
+            // we can use Inkwell also performing actions
+            onLongPress: () => showSheet(context),
+            child: Image.asset("assets/images/megumi.jpg")),
+      ),
     );
   }
 }
 
 void showSheet(BuildContext context) {
-  showModalBottomSheet(context: context, builder: (context) {return Container(child: Column(mainAxisSize: MainAxisSize.min,),)});
+  showBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                leading: FaIcon(
+                  FontAwesomeIcons.whatsapp,
+                  color: Colors.green,
+                ),
+              ),
+              ListTile(
+                leading: FaIcon(
+                  FontAwesomeIcons.facebook,
+                  color: Colors.blue,
+                ),
+              ),
+              ListTile(
+                leading: FaIcon(
+                  FontAwesomeIcons.message,
+                  color: Colors.black,
+                ),
+              )
+            ],
+          ),
+        );
+      });
 }
