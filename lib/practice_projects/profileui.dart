d0bb59c78e2d50f12f959be201ddd36170cb2747
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-08-16 10:37:16 +0530

    Initial Commit

--- lib/practice_projects/profileui.dart
+++ lib/practice_projects/profileui.dart
@@ -2,23 +2,38 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: ProfileUi(),
   ));
 }
 
 class ProfileUi extends StatelessWidget {
-  ProfileUi({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-          backgroundColor: Color(0xFFFFFFFF),
-          //elevation: 0,
-          leading: Icon(
-            Icons.arrow_circle_left_sharp,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.black,
+            ),
+            onPressed: () {},
+          ),
+        ],
+        //backgroundColor: const Color(0xFFFFFFFF),
+        //elevation: 0,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            //CustomIcons.arrow_back,
+            //Icons.arrow_circle_left_sharp,
             color: Colors.black,
-          )),
+          ),
+          onPressed: () {},
+        ),
+      ),
     );
   }
 }
