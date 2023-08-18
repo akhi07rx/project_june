e30ba87472135f7dac22b9e07bf08641a4809c87
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-08-18 13:49:37 +0530

    Initial Commit

--- lib/practice_projects/temp.dart
+++ lib/practice_projects/temp.dart
@@ -1,96 +1,85 @@
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
-
-// void main() {
-//   runApp(MaterialApp(
-//     debugShowCheckedModeBanner: false,
-//     home: Musicx(),
-//   ));
-// }
-
-// class Musicx extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: const Color(0xFF000000),
-//       appBar: AppBar(
-//         centerTitle: true,
-//         backgroundColor: const Color(0xFF000000),
-//         //#000000
-//         title: Text(
-//           'Playlist',
-//           style: GoogleFonts.ubuntu(
-//             //textStyle: Theme.of(context).textTheme.headline6,
-//             fontSize: 30,
-//             color: const Color(0xFF6b7dff),
-//             //fontWeight: FontWeight.w700,
-//             // fontStyle: FontStyle.italic,
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Slivers(),
+    debugShowCheckedModeBanner: false,
+    home: Musicx(),
   ));
 }
 
-class Slivers extends StatelessWidget {
+class Musicx extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF000000),
-      body: SafeArea(
-          child: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            floating: true,
-            pinned: false,
-            title: const Text('Sliver Appbar'),
-            bottom: AppBar(
-              centerTitle: true,
-              backgroundColor: const Color(0xFF000000),
-              elevation: 0,
-              title: Container(
-                width: double.infinity,
-                height: 40,
-                color: Colors.white,
-                child: const TextField(
-                  decoration: InputDecoration(
-                      hintText: "Search here",
-                      fillColor: Colors.black,
-                      prefixIcon: Icon(Icons.search_rounded),
-                      suffixIcon: Icon(Icons.camera_alt)),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: const Color(0xFF000000),
+        //#000000
+        title: Text(
+          'Playlist',
+          style: GoogleFonts.ubuntu(
+            //textStyle: Theme.of(context).textTheme.headline6,
+            fontSize: 30,
+            color: const Color(0xFF6b7dff),
+            //fontWeight: FontWeight.w700,
+            // fontStyle: FontStyle.italic,
+          ),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Container(
+              height: 200,
+              width: 200,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10.0),
+                child: Image.network(
+                  'https://picsum.photos/250?image=$index',
+                  fit: BoxFit.cover,
                 ),
               ),
             ),
-          ),
-          // SliverList(
-          //     delegate: SliverChildBuilderDelegate(
-          //         (context, index) => Container(
-          //               decoration: BoxDecoration(
-          //                   borderRadius: BorderRadius.circular(40)),
-          //               child: Text('Data $index'),
-          //               height: 50,
-          //             ),
-          //         childCount: 10)),
-          SliverGrid(
-              delegate:
-                  SliverChildBuilderDelegate((context, index) => const Card(
-                        child: Center(
-                          child: Icon(Icons.ac_unit_rounded),
-                        ),
-                      )),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4))
-        ],
-      )),
+          );
+        },
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              child: Image.network('https://picsum.photos/250?image=11',
+                  height: 24, width: 24),
+              padding: const EdgeInsets.all(12),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(12),
+                      bottomLeft: Radius.circular(12))),
+              child: Text("Student"),
+              padding: const EdgeInsets.all(12),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
