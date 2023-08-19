dc079718ba0252edb7811da15f9241ba0d118e24
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-08-19 10:19:04 +0530

    Initial Commit

--- lib/class work/passing data between screens/using navigator/using constructor/datapage.dart
+++ lib/class work/passing data between screens/using navigator/using constructor/datapage.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:project_june1/class%20work/passing%20data%20between%20screens/using%20navigator/using%20constructor/to_statlesspage.dart';
 
+
+void main() {
+  runApp(MaterialApp(
+    home: DataPage(),
+  ));
+}
+
 class DataPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Passing Data between sceens using constructor"),
+        title: Text("Passing data between screens using constructor"),
       ),
       body: Container(
         alignment: Alignment.center,
-        height: 250,
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          ElevatedButton(onPressed: () {Navigator.of(context).push(MaterialPageRoute(builder: (context) => StatelessPage()))}, child: Text("To Stateless Page")),
-          ElevatedButton(onPressed: () {}, child: Text("To Stateful Page")),
-        ]),
+        height: 200,
+        child: Column(
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => StatelessPage(
+                          name: "Luminar",
+                          location: "Kakkanad",
+                          phone: 9947837497)));
+                },
+                child: Text('To Stateless Page')),
+                const SizedBox(height: 20,)
+            ElevatedButton(onPressed: () {}, child: Text('To Stateful Page'))
+          ],
+        ),
       ),
     );
   }
