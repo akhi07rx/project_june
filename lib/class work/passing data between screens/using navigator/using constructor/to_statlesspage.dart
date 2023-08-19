fd2a4cc9a7f4783964909ed7c1bcaaac3e5ea613
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-08-19 10:11:56 +0530

    Initial Commit

--- lib/class work/passing data between screens/using navigator/using constructor/to_statlesspage.dart
+++ lib/class work/passing data between screens/using navigator/using constructor/to_statlesspage.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class StatelessPage extends StatelessWidget {
-  const StatelessPage({super.key, required String Name, required String name});
+  const StatelessPage(
+      {super.key, String this.name, String this.location, int this.phone});
 
   @override
   Widget build(BuildContext context) {
