ab832baec3c78fa9e0d7aa39e947ea32ea879856
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-08-21 14:26:04 +0530

    Initial Commit

--- lib/class work/introscreen/onboarding screen.dart
+++ lib/class work/introscreen/onboarding screen.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
+//import 'package:project_june1/class%20work/flutter/splash2.dart';
+import 'package:project_june1/class%20work/flutter/splash3.dart';
+import 'package:project_june1/class%20work/flutter/stateful%20login%20with%20validation.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: IntroScreen(),
+  ));
+}
 
 class IntroScreen extends StatelessWidget {
   const IntroScreen({super.key});
@@ -27,6 +36,20 @@ class IntroScreen extends StatelessWidget {
             image: IntroImage(
                 "https://images.unsplash.com/photo-1531525727990-67532cd332c6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80")),
       ],
+      onDone: () => Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => Splash3())),
+      onSkip: () => Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => Login2())),
+      skip: Text('Skip'),
+      next: Icon(Icons.arrow_forward_ios_rounded),
+      done: Text("Done"),
+      dotsDecorator: DotsDecorator(
+          size: Size(10, 12),
+          color: Colors.grey,
+          activeSize: Size(25, 12),
+          activeColor: Colors.yellow,
+          activeShape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20)))),
     );
   }
 
