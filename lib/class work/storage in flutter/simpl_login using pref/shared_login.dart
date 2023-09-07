49756d811bb1f3a41c2eaa5210e50d29732398f6
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-09-07 13:50:37 +0530

    Initial Commit

---
+++ lib/class work/storage in flutter/simpl_login using pref/shared_login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginShared extends StatefulWidget {
+  const LoginShared({super.key});
+
+  @override
+  State<LoginShared> createState() => _LoginSharedState();
+}
+
+class _LoginSharedState extends State<LoginShared> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import path_provider_foundation
 import rive_common
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
